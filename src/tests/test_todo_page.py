f7e5de61fb08fb47ee22a64ef8185f2727e2ab32
shanecookofficial <shanecookofficial@gmail.com>
2025-11-08 19:38:19 -0600

    Ruff fix

--- src/tests/test_todo_page.py
+++ src/tests/test_todo_page.py
@@ -1,5 +1,4 @@
 from src.pages.todo_page import TodoPage
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
